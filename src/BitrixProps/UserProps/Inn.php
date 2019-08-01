1910aaaddc4062ea67bd39c99f03d4ac5225c00f
frolov <sergeyfolov@inetsys.ru>
2019-08-01 20:08:33 +0300

    [master] add check inn

--- src/BitrixProps/UserProps/Inn.php
+++ src/BitrixProps/UserProps/Inn.php
@@ -1,4 +1,5 @@
 <?php namespace Vf92\BitrixProps\UserProps;
+
 use WebArch\BitrixUserPropertyType\Abstraction\Custom\CheckableValueInterface;
 use WebArch\BitrixUserPropertyType\Abstraction\Custom\ConvertibleValueInterface;
 use WebArch\BitrixUserPropertyType\Abstraction\DbColumnType\StringColTypeTrait;
@@ -141,11 +142,28 @@ END;
      */
     public static function checkFields($userField, $value)
     {
-        if(1 === $value['VALUE']){
+        if (!static::validInn($value['VALUE'])) {
             return [
-                [1, 'Не корректный ИНН']
+                [1, 'Не корректный ИНН'],
             ];
         }
         return [];
+    }
+
+    protected static function validInn($inn)
+    {
+        if (preg_match('/\D/', $inn)) {
+            return false;
+        }
+        $inn = (string)$inn;
+        $len = strlen($inn);
+        if ($len === 10) {
+            return $inn[9] === (string)(((2 * $inn[0] + 4 * $inn[1] + 10 * $inn[2] + 3 * $inn[3] + 5 * $inn[4] + 9 * $inn[5] + 4 * $inn[6] + 6 * $inn[7] + 8 * $inn[8]) % 11) % 10);
+        } elseif ($len === 12) {
+            $num10 = (string)(((7 * $inn[0] + 2 * $inn[1] + 4 * $inn[2] + 10 * $inn[3] + 3 * $inn[4] + 5 * $inn[5] + 9 * $inn[6] + 4 * $inn[7] + 6 * $inn[8] + 8 * $inn[9]) % 11) % 10);
+            $num11 = (string)(((3 * $inn[0] + 7 * $inn[1] + 2 * $inn[2] + 4 * $inn[3] + 10 * $inn[4] + 3 * $inn[5] + 5 * $inn[6] + 9 * $inn[7] + 4 * $inn[8] + 6 * $inn[9] + 8 * $inn[10]) % 11) % 10);
+            return $inn[11] === $num11 && $inn[10] === $num10;
+        }
+        return false;
     }
 }
