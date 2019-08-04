b1eff7f7cfac0915c60d90084f6d1b2a94443b2f
Sergey Frolov <frolov@adv.ru>
2019-08-04 16:20:47 +0300

    [master] update props

--- src/BitrixProps/UserProps/User.php
+++ src/BitrixProps/UserProps/User.php
@@ -1,126 +1,104 @@
 <?php namespace Vf92\BitrixProps\UserProps;
 
+use WebArch\BitrixUserPropertyType\Abstraction\Custom\CheckableValueInterface;
 use WebArch\BitrixUserPropertyType\Abstraction\Custom\ConvertibleValueInterface;
 use WebArch\BitrixUserPropertyType\Abstraction\DbColumnType\IntegerColTypeTrait;
 use WebArch\BitrixUserPropertyType\Abstraction\UserTypeBase;
 
 /** @todo multiple */
-class User extends UserTypeBase implements ConvertibleValueInterface
+class User extends UserTypeBase implements ConvertibleValueInterface, CheckableValueInterface
 {
     use IntegerColTypeTrait;
 
     /**
-     * Эта функция вызывается перед сохранением значений в БД.
-     *
-     * <p>Вызывается из метода Update объекта $USER_FIELD_MANAGER.</p>
-     * <p>Для множественных значений функция вызывается несколько раз.</p>
-     *
-     * @param array $userField Массив описывающий поле.
-     * @param mixed $value     Значение.
-     *
-     * @return string значение для вставки в БД.
+     * @inheritDoc
      */
     public static function onBeforeSave($userField, $value)
     {
-        // TODO: Implement onBeforeSave() method.
+        return $value;
     }
 
     /**
-     * Вызывается после извлечения значения из БД.
-     *
-     * @param array $userField Массив описывающий поле.
-     * @param array $rawValue  ['VALUE' => <актуальное значение>]
-     *
-     * @return array
+     * @inheritDoc
      */
     public static function onAfterFetch($userField, $rawValue)
     {
-        // TODO: Implement onAfterFetch() method.
+        $user = \Bitrix\Main\UserTable::getById($rawValue)->fetchObject();
+        return ['id' => $rawValue, 'user' => $user];
     }
 
     /**
-     * Возвращает базовый тип на котором будут основаны операции фильтра (int, double, string, date, datetime)
-     *
-     * @return string
+     * @inheritDoc
      */
     public static function getBaseType()
     {
-        // TODO: Implement getBaseType() method.
+        return self::BASE_TYPE_INT;
     }
 
     /**
-     * Возвращает описание для показа в интерфейсе (выпадающий список и т.п.)
-     *
-     * @return string
+     * @inheritDoc
      */
     public static function getDescription()
     {
-        // TODO: Implement getDescription() method.
+        return 'Привязка к пользователю';
     }
 
     /**
-     * Эта функция вызывается при выводе формы редактирования значения свойства.
-     *
-     * <p>Возвращает html для встраивания в ячейку таблицы.
-     * в форму редактирования сущности (на вкладке "Доп. свойства")</p>
-     * <p>Элементы $htmlControl приведены к html безопасному виду.</p>
-     *
-     * @param array $userField   Массив описывающий поле.
-     * @param array $htmlControl Массив управления из формы. Содержит элементы NAME и VALUE.
-     *
-     * @return string HTML для вывода.
+     * @inheritDoc
      */
     public static function getEditFormHTML($userField, $htmlControl)
     {
-        // TODO: Implement getEditFormHTML() method.
+        $user = $htmlControl['VALUE']['user'];
+        return '';
     }
 
     /**
-     * Эта функция вызывается при выводе значения свойства в списке элементов.
-     *
-     * <p>Возвращает html для встраивания в ячейку таблицы.</p>
-     * <p>Элементы $arHtmlControl приведены к html безопасному виду.</p>
-     *
-     * @param array $arUserField   Массив описывающий поле.
-     * @param array $arHtmlControl Массив управления из формы. Содержит элементы NAME и VALUE.
-     *
-     * @return string HTML для вывода.
+     * @inheritDoc
      */
     public static function getAdminListViewHtml($userField, $htmlControl)
     {
-        // TODO: Implement getAdminListViewHtml() method.
+        /** @var \Bitrix\Main\EO_User $user */
+        $user = $htmlControl['VALUE']['user'];
+        return '['.$user->getId().'] '.$user->getLastName().' '.$user->getName();
     }
 
     /**
-     * Эта функция вызывается при выводе формы настройки свойства.
-     *
-     * <p>Возвращает html для встраивания в 2-х колоночную таблицу.
-     * в форму usertype_edit.php</p>
-     * <p>т.е. tr td bla-bla /td td edit-edit-edit /td /tr </p>
-     *
-     * @param array $userField
-     * @param array $htmlControl
-     * @param bool  $isVarsFromForm
-     *
-     * @return string
+     * @inheritDoc
      */
     public static function getSettingsHTML($userField, $htmlControl, $isVarsFromForm)
     {
-        // TODO: Implement getSettingsHTML() method.
+        return <<<END
+        <tr>
+            <td>&nbsp;</td>
+            <td><p>Тип свойства "Привязка к пользователю" </p></td>
+        </tr>
+END;
     }
 
     /**
-     * Эта функция вызывается перед сохранением метаданных свойства в БД.
-     *
-     * <p>Она должна "очистить" массив с настройками экземпляра типа свойства.
-     * Для того что бы случайно/намеренно никто не записал туда всякой фигни.</p>
-     *
-     * @param array $userField Массив описывающий поле. <b>Внимание!</b> это описание поля еще не сохранено в БД!
-     *
-     * @return array Массив который в дальнейшем будет сериализован и сохранен в БД.
+     * @inheritDoc
      */
     public static function prepareSettings($userField)
     {
-        // TODO: Implement prepareSettings() method.
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function checkFields($userField, $value)
+    {
+        if ((int)$value['VALUE'] < 1) {
+            return [
+                [1, 'Не корректный id пользователя'],
+            ];
+        }
+        $count = \Bitrix\Main\UserTable::getCount((new \Bitrix\Main\ORM\Query\Filter\ConditionTree())->where('ID', $value['VALUE']));
+        if($count === 0){
+            return [
+                [2, 'Пользователь с данным id пользователя не найден'],
+            ];
+        }
+        return [];
     }
 }
